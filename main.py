27ab1bc12c5d6a3ff00e2f57e5d1b3939a682646
varun-aj <92260963+varun-aj@users.noreply.github.com>
2024-08-15 21:17:17 -0400

    Add files via upload
    added initial files

---
+++ main.py
@@ -0,0 +1,8 @@
+from testdata import TestDataGen
+
+def main():
+   gen = TestDataGen()
+   gen.datagen()
+
+if __name__=="__main__":
+   main()
---
+++ testdata.py
@@ -0,0 +1,44 @@
+import timeit #find the execution time taken by the given code snippet
+import pyspark.sql.functions as f
+import uuid #128-bit number used to uniquely identify information in computer systems
+import random #generates random floating numbers in the range of 0.1, and 1.0
+import string #The Python string module provides several constants that are useful for checking to see if a character, slice, or string contains letters, digits, symbols, etc
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DateType, DecimalType
+from datetime import datetime, timedelta
+import pyspark
+from pyspark.sql import SparkSession
+
+
+class TestDataGen:
+    def __init__(self):
+        self.spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
+
+    def generate_name(self):
+        length=5
+        VOWELS = "aeiou"
+        CONSONANTS = ''.join(set(string.ascii_lowercase) - set(VOWELS))
+        word = ""
+        
+        for i in range(length):
+            if i % 2 == 0:
+                word += random.choice(CONSONANTS)
+            else:
+                word += random.choice(VOWELS)
+        
+        return word
+        
+    def datagen(self):
+        for i in range(1,10):
+            data= [({"order_id":''.join(random.choice(string.ascii_letters) for i in range(8)),
+                    "customer_order_id":''.join(random.choice(string.ascii_letters + string.digits) for i in range(12)),
+                    "tracking_number":''.join(random.choice(string.digits) for i in range(10)),
+                    "Part_name":f"{self.generate_name()}",
+                    "Quantity":random.randint(1,20),
+                    "cost":random.randint(1,999),
+                    "Pack_Date":datetime(2022, 1, 1) + timedelta(days=random.randint(0, (datetime(2024, 12, 31) - datetime(2022, 1, 1)).days)),
+                    "Delivery_Date":datetime(2022, 1, 1) + timedelta(days=random.randint(0, (datetime(2024, 12, 31) - datetime(2022, 1, 1)).days)), 
+                    "Phone":''.join(random.choice(string.digits) for i in range(10)),
+                    "Email":random.choice(f"{''.join(random.choices(string.ascii_letters + string.digits, k=8))}@example.com"),
+                    "Pincode":''.join(random.choices(string.ascii_uppercase + string.digits, k=3)) + ' ' + ''.join(random.choices(string.digits + string.ascii_uppercase, k=3))})]
+        df = spark.createDataFrame(data)
+        df.write.mode("append").format("delta").saveAsTable("test_table5")
